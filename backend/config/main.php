44bd7199cf808f17ecd2477d1c02afafd2e9be1d
HomeSSD <mr_garik@mail.ru>
2019-04-22 01:42:52 +0600

    1

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,7 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
-    'homeUrl' => ' ',
+    'homeUrl' => '',
     'components' => [
         'request' => [
             'baseUrl' => '/admin',
